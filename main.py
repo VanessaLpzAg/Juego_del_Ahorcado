4594d20e7b444fbf6ca0fae6486d04c276d1d182
Vanessalop <vanessalop88@gmail.com>
2025-01-22 10:18:15 +0100

    v_2

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-from utils import *
-if __name__=="__main__":
+from utils import inicializar_juego
+
+def jugar_ahorcado():
+    
+    palabra_adivinar = "python"
+    vidas, palabra_oculta = inicializar_juego(palabra_adivinar)
+
+    print("Palabra a adivinar:", " ".join(palabra_oculta))
+
+    while vidas > 0:
+        letra_usuario = input("Introduce una letra: ").lower()
+
+        if letra_usuario in palabra_adivinar:
+            print(f"¡Has acertado! La letra '{letra_usuario}' está en la palabra.")
+            for index, letra in enumerate(palabra_adivinar):
+                if letra == letra_usuario:
+                    palabra_oculta[index] = letra_usuario
+            print("Progreso:", " ".join(palabra_oculta))
+        else:
+            vidas -= 1
+            print(f"Has fallado. La letra '{letra_usuario}' no está en la palabra.")
+            print(f"Te quedan {vidas} vidas.")
+
+        if "_" not in palabra_oculta:
+            print(f"¡Felicidades! Has adivinado la palabra: {palabra_adivinar}")
+            break
+
+    if "_" in palabra_oculta:
+        print(f"FIN DEL JUEGO. Has perdido. La palabra era: {palabra_adivinar}") 
+
+if __name__ == "__main__":
     jugar_ahorcado()
--- utils.py
+++ utils.py
@@ -1,25 +1,6 @@
-def jugar_ahorcado(vidas, palabra_adivinar, palabra_oculta):
 
-    vidas = 1
-    palabra_adivinar = "python"
-    palabra_oculta = list("_"*len(palabra_adivinar))
-    print(palabra_oculta)
-
-    while vidas > 0:
-        letra_usuario = input("Introduce una letra:")     # Introduccion de letra por parte del usuario.
-    
-        if letra_usuario in palabra_adivinar:             # Comprobacion de la letra introducida con las letras de la palabra a adivinar.
-            print(f"Has acertado. La letra '{letra_usuario}' está en la palabra.")
-        
-           
-        else:
-            print(f"Has fallado. La letra '{letra_usuario}' no esta en la palabra")
-            print("Te quedan:", vidas - 1)
-            print(palabra_oculta)
-        break
-        
-    if palabra_adivinar == palabra_oculta:                # Desarrollo del fin de juego
-        print(f" FIN DEL JUEGO. YOU WIINN!!! La palabra era '{palabra_adivinar}'")
-
-    else:
-        print(f"FIN DEL JUEGO. YOU LOSEEE... La palabra era '{palabra_adivinar}'") 
+def inicializar_juego(palabra):
+    """Inicializa los valores del juego."""
+    vidas = 6
+    palabra_oculta = list("_" * len(palabra))
+    return vidas, palabra_oculta
